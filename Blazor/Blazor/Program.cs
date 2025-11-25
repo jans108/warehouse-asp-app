78e5944669497e6c37a951edc47c27c94dfff568
Wiktor <wiktorbarczyk1@gmail.com>
2025-11-25 18:58:32 +0100

    Początek
    ShoppingCart+

--- Blazor/Blazor/Program.cs
+++ Blazor/Blazor/Program.cs
@@ -1,6 +1,7 @@
 using Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
